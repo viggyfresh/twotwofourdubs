234df7e7c3e06893eb0bb11d60255d57a3d9774a
Dan Guo <danguo1113@gmail.com>
2015-11-15 14:49:03 -0800

    createusergraph

--- createUserGraph.py
+++ createUserGraph.py
@@ -1,8 +1,8 @@
 from collections import defaultdict
 import pickle
 
-posts = pickle.load("pickle/posts.pickle")
-users = pickle.load("pickle/users.pickle")
+posts = pickle.load(open("pickle/posts.pickle",'r'))
+#users = pickle.load("pickle/users.pickle")
 
 edges = defaultdict(list)
 
@@ -11,3 +11,8 @@ for post in posts:
 		if post["PostTypeId"] == "2":
 			edges[post["ParentID"]["OwnerUserId"]].append(post["OwnerUserId"])
 
+with open('parsed/userGraph.csv','w') as outfile:
+	for src_id in edges:
+		dst_ids = edges[src_id]
+		for dst_id in dst_ids:
+			outfile.write("{0};{1}\n".format(src_id, dst_id))
