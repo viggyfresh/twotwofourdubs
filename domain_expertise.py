e758bf852994482ed7174c35314e74406bc2f986
Vignesh Venkataraman <sendittovignesh@gmail.com>
2015-12-05 10:50:09 -0800

    Update centrality features, domain_expertise subtle changes

--- domain_expertise.py
+++ domain_expertise.py
@@ -7,9 +7,9 @@ import operator
 # Params: reputation, badges, UpVotes, DownVotes
 # Computed params:  in/out degree, centrality score
 def domain_expertise(reputation, badges, upvotes, downvotes, indegree, outdegree, centrality):
-    # return float(reputation)
-    # return float(indegree)
-    return (float(indegree) - float(outdegree)) * (float(upvotes) - float(downvotes))
+    return float(reputation)
+    # return float(indegree) - float(outdegree)
+    # return (float(indegree) - float(outdegree)) * (float(upvotes) - float(downvotes))
 
 g = snap.LoadEdgeList(snap.PNGraph, "parsed/geometry_qa.csv", 0, 1, ';')
 ug = snap.ConvertGraph(snap.PUNGraph, g)
@@ -43,4 +43,4 @@ experts = sorted(expertise.items(), key=operator.itemgetter(1), reverse=True)
 for i in xrange(10):
     id = experts[i][0]
     expertise = experts[i][1]
-    print "User id %s, name %s, score %f" % (id, users[id]["DisplayName"], expertise)
+    print "%s - %s - %f" % (id, users[id]["DisplayName"], expertise)
